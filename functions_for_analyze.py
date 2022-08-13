98c9ea309c58a1c76fb4c196f18a164759d247dd
artem-chigin <chiartftc@gmail.com>
2022-08-13 16:09:22 +0400

    Add arguments name check, variables in function name check and arguments value is mutable check. Rewrite snake case check function.

--- functions_for_analyze.py
+++ functions_for_analyze.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import re
+import ast
 
 
 class Symbols(Enum):
@@ -14,6 +15,9 @@ class Symbols(Enum):
     DOUBLE_QUOTES = '"'
     CLASS = "class"
     DEF = "def"
+    FUNC = "function"
+    ARG = "argument"
+    VAR = "variable"
 
 
 class Errors(Enum):
@@ -26,8 +30,11 @@ class Errors(Enum):
     TO_DO_FOUND = "S005 Find TODO in line"
     MORE_THAN_TWO_BLANK_LINES = "S006 More than two blank lines used before this line"
     TOO_MANY_SPACES_AFTER = "S007 Too many spaces after class_or_def"
-    NOT_CAMEL_CASE = "S008 Class name 'class_name' should use CamelCase"
-    NOT_SNAKE_CASE = "S009 Function name 'function_name' should use snake_case"
+    CLASS_NAME_NOT_CAMEL_CASE = "S008 Class name 'class_name' should use CamelCase"
+    FUNCTION_NAME_NOT_SNAKE_CASE = "S009 Function name 'function_name' should use snake_case"
+    ARG_NAME_NOT_SNAKE_CASE = "S010 Argument name 'arg_name' should be snake_case"
+    VAR_NAME_NOT_SNAKE_CASE = "S011 Variable 'var_name' should be snake_case"
+    ARG_VALUE_IS_MUTABLE = "S012 Default argument value is mutable"
 
     MSG = "Error message"
 
@@ -108,7 +115,7 @@ def spaces_after_name_check(path: str, line_value: str, line_num: int) -> Errors
 def camel_case_check(path: str, line_value: str, line_num: int) -> Errors.MSG.value:
     if Symbols.CLASS.value in line_value:
         class_name = line_value.replace("class", Symbols.EMPTY.value).replace(":", Symbols.EMPTY.value).strip()
-        message = Errors.NOT_CAMEL_CASE.value.replace("class_name", class_name)
+        message = Errors.CLASS_NAME_NOT_CAMEL_CASE.value.replace("class_name", class_name)
         # template = r"class [\s]*[A-Z][a-z]+[A-Z]?[a-z]:"
         camel_template = r"[\s][A-Z][a-z]+[A-Z]?[a-z]+"
         check = re.search(camel_template, line_value)
@@ -120,11 +127,30 @@ def snake_case_check(path: str, line_value: str, line_num: int) -> Errors.MSG.va
     if Symbols.DEF.value in line_value:
         def_name = line_value.replace("def", Symbols.EMPTY.value)
         def_name = def_name[: def_name.find("(")].strip()
-        message = Errors.NOT_SNAKE_CASE.value.replace("function_name", def_name)
+        message = Errors.FUNCTION_NAME_NOT_SNAKE_CASE.value.replace("function_name", def_name)
         snake_template = r"[\s]_?_?([a-z]+)_?([a-z\d]*)_?_?[(]"
         snake_check = re.search(snake_template, line_value)
         if not snake_check:
             return create_message(path, line_num, message)
+
+
+def new_snake_case_check(path: str, name: str, line_num: int, type_of_name: str) -> Errors.MSG.value:
+    if type_of_name == Symbols.FUNC.value:
+        message = Errors.FUNCTION_NAME_NOT_SNAKE_CASE.value.replace("function_name", name)
+    elif type_of_name == Symbols.ARG.value:
+        message = Errors.ARG_NAME_NOT_SNAKE_CASE.value.replace("arg_name", name)
+    elif type_of_name == Symbols.VAR.value:
+        message = Errors.VAR_NAME_NOT_SNAKE_CASE.value.replace("var_name", name)
+    template = r"\b_?_?[\da-z_]+_?_?\b"
+    check = re.match(template, name)
+    if not check:
+        return create_message(path, line_num, message)
+
+
+def mutable_args_check(path: str, data, line_num: int) -> Errors.MSG.value:
+    if type(data) not in {int, str, bool, tuple, frozenset} and data is not None:
+        message = Errors.ARG_VALUE_IS_MUTABLE.value
+        return create_message(path, line_num, message)
 
 
 def code_analyze(path: str):
@@ -132,6 +158,24 @@ def code_analyze(path: str):
         line_counter = 1
         path_for_a_message = path[2:]
         content = file.readlines()
+
+        tree = ast.parse("".join(content))
+        # print(ast.dump(tree))
+        function_names = {}
+        argument_names = {}
+        node = ast.walk(tree)
+        var_names = {}
+        arg_types = {}
+
+        for n in node:
+            if isinstance(n, ast.FunctionDef):
+                function_names[n.lineno] = n.name
+                argument_names[n.lineno] = [a.arg for a in n.args.args]
+                arg_types[n.lineno] = [ast.literal_eval(i) for i in n.args.defaults]
+                vars = [i.targets for i in n.body if isinstance(i, ast.Assign)]
+                for var_name in vars:
+                    if isinstance(var_name[0], ast.Name):
+                        var_names[var_name[0].lineno] = var_name[0].id
 
         for line in content:
             length_check_result = length_check(path_for_a_message, line, line_counter)
@@ -166,8 +210,33 @@ def code_analyze(path: str):
             if result_camel_case:
                 print(result_camel_case)
 
-            result_snake_case = snake_case_check(path_for_a_message, line, line_counter)
-            if result_snake_case:
-                print(result_snake_case)
+            # result_snake_case = snake_case_check(path_for_a_message, line, line_counter)
+            # if result_snake_case:
+            #     print(result_snake_case)
+
+            if line_counter in function_names.keys():
+                result_new = new_snake_case_check(path_for_a_message, function_names[line_counter], line_counter, Symbols.FUNC.value)
+                if result_new:
+                    print(result_new)
+
+            if line_counter in argument_names.keys():
+                for arg in argument_names[line_counter]:
+                    result = new_snake_case_check(path_for_a_message, arg, line_counter, Symbols.ARG.value)
+                    if result:
+                        print(result)
+                        break
+
+            if line_counter in var_names.keys():
+                # for name in var_names[line_counter]:
+                result = new_snake_case_check(path_for_a_message, var_names[line_counter], line_counter, Symbols.VAR.value)
+                if result:
+                    print(result)
+
+            if line_counter in arg_types.keys():
+                for data in arg_types[line_counter]:
+                    # print(data)
+                    check = mutable_args_check(path_for_a_message, data, line_counter)
+                    if check:
+                        print(check)
 
             line_counter += 1
